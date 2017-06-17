6bbdd304451bfa0e6c22ac0e90049f27c346bb43
brosla <d.bros@lifebeat-ru>
2017-06-17 11:50:53 +0300

    code cleaning

--- src/app/user-profile/user-profile.ctrl.js
+++ src/app/user-profile/user-profile.ctrl.js
@@ -1,13 +1,9 @@
 class UserProfileController {
-    constructor(user/*, userPosts*/) {
+    constructor(user) {
+        'ngInject';
         this.user = user[0];
-        // this.userPosts = userPosts;
-
         console.log(this.user);
     }
-
-
-
 }
 
 export default UserProfileController;
